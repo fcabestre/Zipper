54fab72c8153bd82ea6929b1bda4aec98f331d05
Frédéric Cabestre <frederic.cabestre@free.fr>
2012-10-04 09:12:09 +0200

    Implenting all the tests for the navigation functions

--- src/test/scala/org/ultimo/zipper/ZipperSuite.scala
+++ src/test/scala/org/ultimo/zipper/ZipperSuite.scala
@@ -10,10 +10,69 @@ class ZipperSuite extends FunSuite {
   val initialTree = Section(List(leftTerm, midTerm, rightTerm))
   val initialLocation = new Location(initialTree, Top)
 
-  test("goDown test") {
-    val loc = new Location(initialTree, Top)
-    val expectedLoc = new Location(leftTerm, Node(Nil, Top, List(midTerm, rightTerm)))
-    val result = loc.goDown
-    assert(result == expectedLoc)
+  test("goDown: nominal") {
+    expect(Location(leftTerm, Node(Nil, Top, List(midTerm, rightTerm)))) {
+      Location(initialTree, Top).goDown
+    }
+  }
+
+  test("goDown: down of item ") {
+    intercept[NoSuchElementException] {
+      Location(Item(""), Top).goDown
+    }
+  }
+
+  test("goDown: down of empty") {
+    intercept[NoSuchElementException] {
+      Location(Section(Nil), Top).goDown
+    }
+  }
+
+  test("goUp: nominal") {
+    expect(initialLocation) {
+      Location(midTerm, Node(List(leftTerm), Top, List(rightTerm))).goUp
+    }
+  }
+
+  test("goUp: up of top") {
+    intercept[NoSuchElementException] {
+      initialLocation.goUp
+    }
+  }
+
+  test("goRight: nominal") {
+    expect(Location(midTerm, Node(List(leftTerm), Top, List(rightTerm)))) {
+      Location(leftTerm, Node(Nil, Top, List(midTerm, rightTerm))).goRight
+    }
+  }
+
+  test("goRight: right of top") {
+    intercept[NoSuchElementException] {
+      initialLocation.goRight
+    }
+  }
+
+  test("goRight: right of last") {
+    intercept[NoSuchElementException] {
+      Location(rightTerm, Node(List(midTerm, leftTerm), Top, Nil)).goRight
+    }
+  }
+
+  test("goLeft: nominal") {
+    expect(Location(leftTerm, Node(Nil, Top, List(midTerm, rightTerm)))) {
+      Location(midTerm, Node(List(leftTerm), Top, List(rightTerm))).goLeft
+    }
+  }
+
+  test("goRight: left of top") {
+    intercept[NoSuchElementException] {
+      initialLocation.goLeft
+    }
+  }
+
+  test("goRight: left of first") {
+    intercept[NoSuchElementException] {
+      Location(leftTerm, Node(Nil, Top, List(midTerm, rightTerm))).goLeft
+    }
   }
 }
